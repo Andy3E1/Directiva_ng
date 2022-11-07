9c1a627266850ae0f77966fb9dd126d94e5a4e98
Andy3E1 <andycap2006@gmail.com>
2022-11-07 08:34:23 -0500

    primer commit

---
+++ src/Components/component_hola1/hola/hola.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HolaComponent } from './hola.component';
+
+
+
+@NgModule({
+  declarations: [HolaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HolaModule { }
